9289b0ac30bf0d7e42ab61eb0ace241889f6c558
Mihail Hristov <mis6us@icloud.com>
2020-07-29 13:16:44 +0300

    add src code

--- src/ExtractEmails.java
+++ src/ExtractEmails.java
@@ -1,9 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ExtractEmails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String regex = "(?<name>[A-Za-z0-9]+[.-_]*[A-Za-z0-9]+)@(?<domain>[A-Za-z]+[-]?[A-Za-z]+[.][A-Za-z]+[.]?[A-Za-z]+)";
+        Pattern pattern = Pattern.compile(regex);
+        List<String> validEmails = new ArrayList<>();
 
+        String input = scanner.nextLine();
+
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()){
+            validEmails.add(matcher.group());
+        }
+
+        validEmails
+                .forEach(e -> System.out.println(e));
     }
 }
